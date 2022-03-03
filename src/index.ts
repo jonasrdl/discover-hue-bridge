6d666e08363d7d4ad65483904950b1f7b69fc9bb
jonasrdl <jonasriedel@protonmail.com>
2022-03-03 10:26:32 +0100

    Add express endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
+import express from 'express';
 
 const URL: string = 'https://discovery.meethue.com/';
+const PORT: number = 6969;
 
 const discoverBridge = async () => {
     return await axios.get(URL);
 };
+
+const app = express();
 
 (async () => {
     let data = await discoverBridge();
@@ -26,9 +30,16 @@ const discoverBridge = async () => {
                     console.log(
                         `The IP of your bridge is: ${result.config.url}`
                     );
-                    process.exit(1);
+
+                    app.get('/bridge/ip', (req, res) => {
+                        res.send({
+                            ip: ip.ip
+                        });
+                    });
                 }
             })
-            .catch();
+            .catch((error) => console.log(error.message));
     });
 })();
+
+app.listen(PORT, () => console.log(`API running on port ${PORT}`));
